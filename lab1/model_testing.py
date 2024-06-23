b66412ebc7e83aff1662762dbc8b83d3ef597feb
MaukhetdinovEF <tore1sk@gmail.com>
2024-06-24 00:28:46 +0500

    Работа 1

---
+++ lab1/model_testing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import joblib
+from sklearn.metrics import mean_squared_error
+
+
+# Загрузка модели
+model = joblib.load('../data/model/model.pkl')
+
+# Загрузка тестовых данных
+test_data = pd.read_csv('../data/test/test_data_preprocessed.csv')
+X_test = test_data[['day']]
+y_test = test_data['temperature']
+
+# Предсказание и оценка модели
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+
+print(f'Mean Squared Error for the test set: {mse}')
